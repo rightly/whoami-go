b2d640141a6b629e1fcdbe15c974b9bc384db0bc
seunghwan <seunghwan.na@navercorp.com>
2018-08-21 18:03:42 +0900

    modified: gc

--- server/dns.go
+++ server/dns.go
@@ -115,7 +115,7 @@ func (s *Server) throw(ldns *net.IP) {
 	s.RequestId <- reqId
 
 	// 1초 후에도 채널에 값이 있는지 확인
-	time.Sleep(1 * time.Second)
+	time.Sleep(500 * time.Nanosecond)
 	s.mu.Lock()
 	select {
 	case id := <-s.RequestId:
